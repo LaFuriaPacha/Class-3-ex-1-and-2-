124f31b26b10a1b737fd0ba82173e21657ef0f1c
Carlos Pacha Pinto <86452932+LaFuriaPacha@users.noreply.github.com>
2022-08-22 20:59:06 -0500

    Add files via upload

---
+++ exercise 1.js
@@ -0,0 +1,5 @@
+function learn(language = "to learn"){
+    console.log(` I love learning ${language}`); // String interpelation
+}
+//function calls (argument in between  a function (here))
+learn("JavaScript");
---
+++ exercise 2.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "Carlos", //Key:Value
+    occupation: "lawyer",
+    age: 28,
+
+};
+// Calling a method on a object
+function currentStudent() {
+    console.log(student.name + " is " + student.age + " and is a " + student.occupation);
+} 
+
